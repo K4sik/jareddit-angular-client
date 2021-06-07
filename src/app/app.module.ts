a78ff4ee33787f001946f3aa46fab94bd838af0f
K4sik <kasiks597@gmail.com>
2021-06-07 10:36:52 +0300

    Added form for create-subreddit and dpost

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,8 @@ import { PostTitleComponent } from './shared/post-title/post-title.component';
 import { SideBarComponent } from './shared/side-bar/side-bar.component';
 import { SubredditSideBarComponent } from './shared/subreddit-side-bar/subreddit-side-bar.component';
 import { VoteButtonComponent } from './shared/vote-button/vote-button.component';
+import { CreateSubredditComponent } from './subreddit/create-subreddit/create-subreddit.component';
+import { CreatePostComponent } from './post/create-post/create-post.component';
 
 @NgModule({
   declarations: [
@@ -28,7 +30,9 @@ import { VoteButtonComponent } from './shared/vote-button/vote-button.component'
     PostTitleComponent,
     SideBarComponent,
     SubredditSideBarComponent,
-    VoteButtonComponent
+    VoteButtonComponent,
+    CreateSubredditComponent,
+    CreatePostComponent
   ],
   imports: [
     BrowserModule,
